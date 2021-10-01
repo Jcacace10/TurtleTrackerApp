6098b7017f3f66651a4b46c07992509e5fd15745
Julie Cacace <jc826@duke.edu>
2021-10-01 13:23:34 -0400

    iteratess through all records

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -19,17 +19,18 @@ line_list= file_object.readlines()
 #close the file
 file_object.close()
 
-#Pretend we read one line of data from the file
-lineString = line_list[100]
-# Split the string into a list of data items
-lineData= lineString.split()
-
-#extract items in list into variables
-record_id= lineData[0]
-obs_date= lineData[2]
-obs_lc= lineData[4]
-obs_lat= lineData[6]
-obs_lon=lineData[7]
-
-#Print the location of Sara
-print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon:{obs_lon} on {obs_date}")
+#Iterate through all lines in the lineList
+for lineString in line_list:
+    if lineString[0] in ("#","u"): continue
+    # Split the string into a list of data items
+    lineData= lineString.split()
+    
+    #extract items in list into variables
+    record_id= lineData[0]
+    obs_date= lineData[2]
+    obs_lc= lineData[4]
+    obs_lat= lineData[6]
+    obs_lon=lineData[7]
+    
+    #Print the location of Sara
+    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon:{obs_lon} on {obs_date}")
